f4c60bdc3bdfd81b0bb11e566fb873771b3af6cc
tharaka27 <tharakasachintha27@gmail.com>
2023-06-05 11:53:19 +0000

    ros arg passing added

--- ros2_test_runner/package.py
+++ ros2_test_runner/package.py
@@ -1,11 +1,16 @@
 class Package:
-    def __init__(self, name: str, launch: str, leader=False):
+    def __init__(self, name: str, launch: str, ros_args:dict, leader=False):
         self.package_name = name
         self.launch_file = launch
         self.leader = leader
+        self.ros_args = ros_args
 
     def getRunCommand(self):
-        return f"ros2 launch {self.package_name} {self.launch_file}"
+        string = ""
+        for key, value in self.ros_args.items():
+            string = key + ":=" + str(value) + " "
+
+        return f"ros2 launch {self.package_name} {self.launch_file} {string}"
     
     def isLeader(self):
         return self.leader
